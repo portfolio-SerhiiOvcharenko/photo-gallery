f6efeefad593ab19b87be53f61424cb159fc130c
Serhii <sergiy20101995@ukr.net>
2017-08-16 02:21:04 +0300

    add .travic.yml fix readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PhotoGallery
+
+If you are going to collaborate or review this project, please check [instructions](https://github.com/portfolio-SerhiiOvcharenko/photo-gallery/wiki/Project-info)
+firstly  
+
+
+The main reason to start working on this project was to improve own knowledge in Angular 2/4.  
+Thank you for spending time to review my code and feel free to write me if you have any remarks or question.   
+Have a nice day :wink:
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Routes} from '@angular/router';
 import { GalleryComponent} from './app/gallery/gallery.component';
 import { ImageDetailComponent} from './app/image-details/image-detail.component';
 
-export const appRoutes:Routes = [
+export const appRoutes: Routes = [
   { path: 'gallery', component: GalleryComponent },
   { path: 'image/:id', component: ImageDetailComponent },
   { path: '', redirectTo: '/gallery', pathMatch: 'full' }
